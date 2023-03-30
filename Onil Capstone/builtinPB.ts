8a911053fe592aad60fad41384ddf914fc4ec782
Alronil Pacheco <alronilpacheco10@gmail.com>
2023-03-29 21:43:33 -0700

    final

--- Onil Capstone/builtinPB.ts
+++ Onil Capstone/builtinPB.ts
@@ -11,10 +11,18 @@ export class Google extends BasePage {
     midlevel: By = By.xpath('(//span[@class="item-title"])[2]')
     aerospace: By = By.xpath('(//span[@class="item-icon"])[3]')
     numemployees: By = By.xpath('(//span[@class="item-title"])[3]')
+    clearfilter: By = By.xpath('//button[@class="filter-control clear-filters-button"]')
+    homepage: By = By.xpath('//img[@class="desktop visible"]')
+    searchbutton: By = By.xpath('//button[@class="menu-search"]')
+    inputsearch: By = By.xpath('//input[@class="search-input"]')
+    viewjobs: By = By.xpath('//a[@href="/jobs"]')
+    remotefilter: By = By.xpath('(//button[@class="bix-dropdown-trigger"])[1]')
+    remoteselected: By = By.xpath('(//span[@class="item-title"])[2]')
+
 
     constructor() {
         super({url: "https://builtin.com/"});
-    }
+    }   
 
     
 }
